e885291c522ca0792e1b6d37181763d8da6331eb
Ajao Malik <ajaomalik2018@gmail.com>
2022-08-23 13:17:29 -0700

    python conditional statement

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for alpha in range(0, len(str)):
+        if ord(str[alpha]) > 96 and ord(str[alpha]) < 123:
+            print("{}".format(chr(ord(str[alpha]) - 32)), end="")
+        else:
+            print("{}".format(str[alpha]), end="")
+    print("")
