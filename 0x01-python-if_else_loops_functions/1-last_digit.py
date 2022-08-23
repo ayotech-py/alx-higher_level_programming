9e2d90111dc1262d7dbb613321916e0c4ab53999
Ajao Malik <ajaomalik2018@gmail.com>
2022-08-23 06:23:21 -0700

    Python conditional statements

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,7 +5,7 @@ if number > 0:
     if int(str(number)[-1]) > 5:
         print(f"Last digit of {number:d} is {str(number)[-1]} and is greater than 5")
     elif int(str(number)[-1]) == 0:
-        print(f"Last digit of {number:d} is {str(number)[-1]} and is zero")
+        print(f"Last digit of {number:d} is {str(number)[-1]} and is 0")
     elif int(str(number)[-1]) < 6 and int(str(number)[-1]) != 0:
         sign = ""
         if str(number)[0] == "-":
@@ -17,4 +17,4 @@ elif number < 0:
     elif -1 * int(str(number)[-1]) < 6 and -1 * int(str(number)[-1]) != 0:
         print(f"Last digit of {number:d} is -{str(number)[-1]} and is less than 6 and not 0")
 elif int(str(number)[-1]) == 0:
-    print(f"Last digit of {number:d} is {str(number)[-1]} and is zero")
+    print(f"Last digit of {number:d} is {str(number)[-1]} and is 0")
