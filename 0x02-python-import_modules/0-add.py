ca81f0060c1e59e92553b9dcca73915297070129
Ajao Malik <ajaomalik2018@gmail.com>
2022-08-25 05:54:58 -0700

    python modules

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,4 +3,4 @@ import add_0 as addme
 a = 1
 b = 2
 if __name__ == "__main__":
-    print("{} + {} = {}".format(a, b, addme.add(a, b)))
+    print("{} + {} = {}".format(a, b, addme.add(a, b)), end="\n")
