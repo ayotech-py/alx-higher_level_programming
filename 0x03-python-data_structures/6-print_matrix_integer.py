07228432c2ee07c656d1f591a83f530bdd9ecba5
Ajao Malik <ajaomalik2018@gmail.com>
2022-08-28 01:58:58 -0700

    Python data strutures

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for nested_row in row:
-            print("{:d}".format(nested_row), end=" ")
+            print("{:d}".format(nested_row), end="")
         print("")
