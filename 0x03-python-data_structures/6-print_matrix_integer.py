1f5d6988723eb218a71c31d5f1f3be6a4e031b48
Ajao Malik <ajaomalik2018@gmail.com>
2022-08-28 02:22:49 -0700

    Python data strutures

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for nested_row in row:
-            if row.index(nested_row) != 2:
-                print("{:d}".format(nested_row), end=" ")
-            else:
-                print("{:d}".format(nested_row), end="")
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if j != 0:
+                print(" ", end='')
+            print("{:d}".format(matrix[i][j]), end='')
         print()
