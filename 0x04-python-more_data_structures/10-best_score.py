1095e99b432d710ce35f5e0cb7da7dd98b7ab2fd
Ajao Malik <ajaomalik2018@gmail.com>
2022-09-03 08:18:15 -0700

    Python more data structures

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None or len(a_dictionary) is 0:
+    if a_dictionary is None or len(a_dictionary) == 0:
         return None
     dic_list = []
     for key in a_dictionary:
