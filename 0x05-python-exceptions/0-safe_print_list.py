ec5fe13efd599796d81160b369a0f4714980b5ed
Ajao Malik <ajaomalik2018@gmail.com>
2022-09-13 23:46:02 -0700

    Python Exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         a = 0
         for i in my_list:
-            if i == a:
+            if x == a:
                 break
             a += 1
             print('{}'.format(i), end='')
