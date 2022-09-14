cca6c758e8dd1a16bae08e53d07dec2063fb0a32
Ajao Malik <ajaomalik2018@gmail.com>
2022-09-14 16:21:27 -0700

    python classes

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This module returns the area of a square"""
+
+
+class Square:
+    """square class"""
+    def __init__(self, size=0):
+        """initialization"""
+        if type(size) != int:
+            raise TypeError("Size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+    def area(self):
+        """this method returns the area"""
+        return self.__size * self.__size
