6265808e962ea32d2767e98a2717465cf07f1d16
Ajao Malik <ajaomalik2018@gmail.com>
2022-09-26 14:32:02 -0700

    Python inheritance

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This file contains isinstance"""
+
+
+def is_same_class(obj, a_class):
+    """This function returns true if obj is an 
+instance of a_class otherwise false"""
+
+    return isinstance(obj, a_class)
