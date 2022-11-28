b7201d03f913de80253f54a5a08ea0c3ea48453e
Ajao Malik <ajaomalik2018@gmail.com>
2022-11-28 15:59:32 -0800

    Javascript warm up

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,9 +2,9 @@
 const process = require('process');
 const arglen = process.argv.length;
 if (arglen === 2) {
-	console.log('No argument');
+	console.log('No argument')
 } else if (arglen === 3) {
-	console.log('Argument found');
+	console.log('Argument found')
 } else {
-	console.log('Arguments found');
+	console.log('Arguments found')
 }
