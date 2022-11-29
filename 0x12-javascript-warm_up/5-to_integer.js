df42af6a052819ede891d710ace691ffc14becb0
Ajao Malik <ajaomalik2018@gmail.com>
2022-11-28 16:29:22 -0800

    Javascript warm up

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const { argv } = require('process');
+
+const num = parseInt(argv[2]) / 1;
+if (num) {
+	console.log('My number is ' + num);
+} else {
+	console.log('Not a number');
+}
+
