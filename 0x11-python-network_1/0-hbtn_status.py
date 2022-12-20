8b0d4d4e215c59d0711710a485ca4f3ae98ed5ee
Ajao Malik <ajaomalik2018@gmail.com>
2022-12-20 08:01:39 -0800

    python networking

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
-
+"""This pythoon script fetches a url"""
 import urllib.request
 
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    res = response.read()
-    print(res)
+
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        res = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(res)))
+        print("\t- content: {}".format(res))
+        print("\t- utf8 content: {}".format(res.decode("utf-8")))
