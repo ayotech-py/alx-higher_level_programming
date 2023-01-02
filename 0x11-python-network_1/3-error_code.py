bc9fa9b3674e6b55b9de88b86338c5ed5f520bf2
ayotech-py <ajaomalik2018@gmai.com>
2023-01-02 03:44:16 -0800

    python networking

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """This file handles urllib.error.HTTPError error"""
-
 from urllib import request, error
 import sys
 
-try:
-    with request.urlopen(sys.argv[1]) as response:
-        print(response.read())
-except error.HTTPError as e:
-    print("Error code: {}".format(e.getcode()))
+
+if __name__ == "__main__":
+    try:
+        with request.urlopen(sys.argv[1]) as response:
+            print(response.read())
+    except error.HTTPError as e:
+        print("Error code: {}".format(e.getcode()))
