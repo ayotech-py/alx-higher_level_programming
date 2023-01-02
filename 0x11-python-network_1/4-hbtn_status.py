578ba58489510a868b4c4a139951aff1174336c8
ayotech-py <ajaomalik2018@gmai.com>
2023-01-02 04:15:14 -0800

    python networking

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This script fetches a url"""
+import requests
+
+if __name__ == "__main__":
+    body = requests.get('https://alx-intranet.hbtn.io/status')
+    print('Body response:')
+    print("\t- type: {}".format(type(body.text)))
+    print("\t- content: {}".format(body.text))
