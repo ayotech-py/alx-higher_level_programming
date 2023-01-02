e891d4a4120da4b8f0df0e6364e4105ca5d56dea
ayotech-py <ajaomalik2018@gmai.com>
2023-01-02 03:59:12 -0800

    Python networking

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -7,6 +7,6 @@ import sys
 if __name__ == "__main__":
     try:
         with request.urlopen(sys.argv[1]) as response:
-            print(response.read())
+            print(response.read().decode())
     except error.HTTPError as e:
         print("Error code: {}".format(e.getcode()))
