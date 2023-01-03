9b64550cae05436803e4923f8a463269b72c7876
ayotech-py <ajaomalik2018@gmai.com>
2023-01-03 01:20:39 -0800

    python networking

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""The script sends a post request to a url"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    body = requests.post(sys.argv[1], data = {'email': sys.argv[2]})
+    print(body.text)
